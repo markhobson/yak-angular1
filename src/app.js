16eb6275ee944ae20b81a057a8ad53badfc3d621
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-01-04 14:16:50 +0000

    Added addYak component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 angular.module('yak', [])
 	.component('app', {
-		templateUrl: 'src/app.html'
+		templateUrl: 'src/app.html',
+		controller: function() {
+			this.addYak = function(name) {
+				console.log('Add yak: ' + name);
+			}
+		}
+	})
+	.component('addYak', {
+		templateUrl: 'src/addYak.html',
+		bindings: {
+			onAdd: '&'
+		}
 	});
